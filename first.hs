2c6577eb85f124cb85fe80c4feab51f591312b39
Mindaugas <ciamusu1@gmail.com>
2015-10-14 22:04:42 +0300

    decoding done for good
    finally

--- first.hs
+++ first.hs
@@ -1,56 +1,55 @@
 import Data.List
+import Data.Char
 import Data.List.Split
---import qualified Data.Text as T
 
 message :: String
 message = "l[m[x 2; y 2; v \"x\"]; m[x 2; y 0; v \"o\"]; m[x 1; y 1; v \"x\"]; m[x 1; y 0; v \"o\"]; m[x 0; y 1; v \"x\"]; m[x 2; y 0; v \"x\"]]"
 
-type Coordinate  = (Char, Char)
-type Value  = Char
-type Coordinates = (Coordinate, Coordinate)
-type InternalMap = (Coordinates, Value)
 type ExternalMap = [InternalMap]
 
-readTillPairEnd :: String -> (String, Coordinate)
-readTillPairEnd encodedMap =
-    let 
-        pair = takeWhileInclusive (/= ';') encodedMap
-        coordinate = drop (length pair - 4) pair
-        rest = drop (length pair) encodedMap
-    in  (rest, (coordinate !! 0, coordinate !! 2))
+data InternalMap = InternalMap { x :: Int  
+                     , y :: Int  
+                     , value :: Char 
+                     } deriving (Show)
 
+readFullMap :: String -> ExternalMap
+readFullMap encodedMap = map readInternalMap $ splitIntoEncodedMaps message
+
+readCoordinates :: String -> (String, Int, Int)
+readCoordinates map =
+    let
+       (rest, coord) = readTillPairEnd map
+       (rest', coord') = readTillPairEnd rest
+    in (rest', coord, coord')
+
+readTillPairEnd :: String -> (String, Int)
+readTillPairEnd encodedMap = 
+    let
+        string = dropWhile (reqNum) encodedMap
+        coord = digitToInt $ head string
+    in (drop 1 string, coord)
+
+readValue :: String -> Char
+readValue restInternalMap = head $ dropWhile (xORo) restInternalMap
 
 readInternalMap :: String -> InternalMap
-readInternalMap encodedMap = 
-	let 
-		(rest, coords) = readCoordinates encodedMap
-		value = readValue rest
-	in (coords, value)
-
-
-readCoordinates :: String -> (String, Coordinates)
-readCoordinates map =
-	let
-	   (rest, coord) = readTillPairEnd map
-	   (rest', coord') = readTillPairEnd rest
-	in (rest', (coord, coord'))
-
-readValue :: String -> Value
-readValue restInternalMap =
+readInternalMap encodedMap =
     let 
-        pair = takeWhileInclusive (/= ';') restInternalMap
-    in  pair !! 4
-
---filter (/=' ') string
-
-takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
-takeWhileInclusive _ [] = []
-takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs
-                                         else []
-
-formatString :: String -> String
-formatString x = drop 2 (filter (/=' ') (init x))
+        (rest, x, y) = readCoordinates encodedMap
+        value = readValue rest
+    in InternalMap x y value
 
 splitIntoEncodedMaps :: String -> [String]
 splitIntoEncodedMaps "" = error "empty string passed"
-splitIntoEncodedMaps s = splitOn "m" formatString message
+splitIntoEncodedMaps s = splitOn "m" (drop 3 (filter (/=' ') (s)))
+
+xORo :: Char -> Bool
+xORo 'x' = False
+xORo 'o' = False
+xORo _ = True
+
+reqNum :: Char -> Bool
+reqNum '0' = False
+reqNum '1' = False
+reqNum '2' = False
+reqNum _ = True

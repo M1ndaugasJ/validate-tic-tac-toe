a35619c2b10701eb58c2903dfeeaae7965b8b4fe
Mindaugas <ciamusu1@gmail.com>
2015-10-14 12:35:02 +0300

    decoding
    refactoring decoding

--- first.hs
+++ first.hs
@@ -1,4 +1,6 @@
 import Data.List
+import Data.List.Split
+--import qualified Data.Text as T
 
 message :: String
 message = "l[m[x 2; y 2; v \"x\"]; m[x 2; y 0; v \"o\"]; m[x 1; y 1; v \"x\"]; m[x 1; y 0; v \"o\"]; m[x 0; y 1; v \"x\"]; m[x 2; y 0; v \"x\"]]"
@@ -39,8 +41,16 @@ readValue restInternalMap =
         pair = takeWhileInclusive (/= ';') restInternalMap
     in  pair !! 4
 
+--filter (/=' ') string
 
 takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
 takeWhileInclusive _ [] = []
 takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs
                                          else []
+
+formatString :: String -> String
+formatString x = drop 2 (filter (/=' ') (init x))
+
+splitIntoEncodedMaps :: String -> [String]
+splitIntoEncodedMaps "" = error "empty string passed"
+splitIntoEncodedMaps s = splitOn "m" formatString message

40b735f8742228e098bcb94c82bc2d9fc4a04240
Mindaugas <ciamusu1@gmail.com>
2015-10-16 21:15:22 +0300

    check if no successive  moves are equal

--- Validate.hs
+++ Validate.hs
@@ -1,11 +1,21 @@
 import First
 import Data.Vector
 import Data.Set
+import Data.List
 
 --value readFullMap message !! 1
-returnCoords :: Int -> String
-returnCoords n = [x (readFullMap !! n)] Prelude.++ [y (readFullMap !! n)]
+returnCoords :: Int -> Int
+returnCoords n = x (readFullMap !! n) + y (readFullMap !! n)
 
  --x = generate (Prelude.length(readFullMap))(\n-> returnCoords n)
+ --Prelude.filter(\n-> n 'x' returnIsValue) readFullMap
+
+--(Prelude.map (\n-> value n) $ readFullMap)
+
+parsedValues = (Prelude.map (\n-> value n) $ readFullMap)
 
 areCollidedValues = Prelude.length (Data.Set.fromList readFullMap) < Prelude.length readFullMap
+areAnySuccessiveMovesEqual = isInfixOf "xx" parsedValues || isInfixOf "oo" parsedValues
+
+returnIsValue :: Int -> Char -> Bool
+returnIsValue n char = value (readFullMap !! n) == char
--- first.hs
+++ first.hs
@@ -5,37 +5,37 @@ import Data.Char
 import Data.List.Split
 
 message :: String
-message = "l[m[x 2; y 1; v \"x\"]; m[x 2; y 1; v \"x\"]; m[x 2; y 1; v \"x\"]]"
+message = "l[m[x 2; y 1; v \"x\"]; m[x 2; y 1; v \"o\"]; m[x 2; y 1; v \"o\"]]"
 
 type ExternalMap = [InternalMap]
-
-data InternalMap = InternalMap { x :: Char  
-                     , y :: Char  
+type Coord = Int
+data InternalMap = InternalMap { x :: Coord  
+                     , y :: Coord  
                      , value :: Char 
-                     } deriving (Show)
+                     } deriving (Show, Ord, Eq)
 
-instance Eq InternalMap where
-    (InternalMap i1 x1 _) == (InternalMap i2 x2 _) = (i1 == i2) && (x1 == x2)
+--instance Eq InternalMap where
+--    (InternalMap i1 x1 _) == (InternalMap i2 x2 _) = (i1 == i2) && (x1 == x2)
     --(InternalMap i1 x1 _) /= (InternalMap i2 x2 _) = (i1 /= i2) || (x1 /= x2)
 
-instance Ord InternalMap where
-    (InternalMap i1 _ _) <= (InternalMap i2 _ _) = (i1 <= i2)
+--instance Ord InternalMap where
+--    (InternalMap i1 _ _) <= (InternalMap i2 _ _) = (i1 <= i2)
 
 readFullMap = map readInternalMap $ splitIntoEncodedMaps message
 
-readCoordinates :: String -> (String, Char, Char)
+readCoordinates :: String -> (String, Coord, Coord)
 readCoordinates singleMap =
     let
        (rest, coord) = readTillPairEnd singleMap
        (rest', coord') = readTillPairEnd rest
     in (rest', coord, coord')
 
-readTillPairEnd :: String -> (String, Char)
+readTillPairEnd :: String -> (String, Coord)
 readTillPairEnd encodedMap = 
     let
         string = dropWhile (reqNum) encodedMap
-        --coord = digitToInt $ head string
-        coord = head string
+        coord = digitToInt $ head string
+        --coord = head string
     in (drop 1 string, coord)
 
 readValue :: String -> Char
